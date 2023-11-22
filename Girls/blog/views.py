36cbf5b721a9efd4aa4696c56627b1314fa9a747
lymex1 <ekarinkin@gmial.com>
2023-11-22 12:10:13 +0300

    Add jinja

--- Girls/blog/views.py
+++ Girls/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/html/blog.html')
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/html/blog.html', {"post": post})
+
