5483450cefe35a74e0beab93b5782614bc4edbd6
lymex1 <ekarinkin@gmial.com>
2023-12-11 18:25:09 +0300

    Add many many fichs

--- Girls/Girls/settings.py
+++ Girls/Girls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%rdsw-zhjz(pf9i#monbanan)bct1u6_^al)=(juj-1$g-0u1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lymex.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- Girls/Girls/urls.py
+++ Girls/Girls/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', views.LoginView.as_view(), name='login')
 ]
